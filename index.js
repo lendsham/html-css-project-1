599dcbb36d31ce649c99617da30c4f3c520fac35
lends <lendsham1st@gmail.com>
2021-08-13 09:29:40 +0700

    Add Dark Mode

--- index.js
+++ index.js
@@ -12,16 +12,33 @@ hamburger.addEventListener('click', () => {
 let darkMode = document.getElementById('darkMode');
 let toggleButton = document.querySelector('.fa-toggle-off');
 let contentBackground = document.getElementById('content');
-let formBackground = document.getElementById('form');
 let contentColorTitle = document.querySelector('#content .content-title');
 let contentColorP = document.querySelector('#content .content .content-top p');
+let formBackground = document.getElementById('form');
+let formColorH1 = document.querySelector('#form .form .form-title');
+let formColorP = document.querySelector('#form .form .form-text');
+
 
 darkMode.addEventListener('click', function() {
+    if(contentColorTitle.style.color === 'gainsboro' && contentColorP.style.color === 'gainsboro') {
+        contentColorTitle.style.color = 'black';
+        contentColorP.style.color = 'black';
+    } else {
+        contentColorTitle.style.color = 'gainsboro';
+        contentColorP.style.color = 'gainsboro';
+    }
+
+    // if(formColorH1.style.color === 'gainsboro' && formColorP.style.color === 'gainsboro') {
+    //     formColorH1.style.color = 'black';
+    //     formColorP.style.color = 'black';
+    // } else {
+    //     formColorH1.style.color = 'gainsboro';
+    //     formColorP.style.color = 'gainsboro';
+    // }
+
         toggleButton.classList.toggle('fa-toggle-on');
         contentBackground.classList.toggle('contentDarkMode');
         formBackground.classList.toggle('formDarkMode');
-        contentColorTitle.style.color = 'gainsboro';
-        contentColorP.style.color = 'gainsboro';
 });
 
 item.forEach(item => {
